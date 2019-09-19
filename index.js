520fb7e70c5f29ff5a759b99da03e250de5070da
bassiebangura <devbassiebangura@gmail.com>
2019-09-18 20:09:22 -0400

    Add holding text for framing project.

---
+++ index.js
@@ -0,0 +1,26 @@
+items = [
+    {
+        name: 'AirMax 90',
+        brand: Nike,
+        description: "Best kdjsai llak kdie is akjfie ksdj lajdlksdjfos ksjdie klaskdjfe klsjadi",
+        shortDescription: "Best kdjsai llak...",
+        imageUrl: "https://placebeard.it/400x400",
+        price: $160,
+        prodId: 18287784847748,
+        rating: 4.3,
+        ratingCount: 89,
+        salePrice: null
+      },
+      {
+        name: 'AirMax 90',
+        brand: Nike,
+        description: "Best kdjsai llak kdie is akjfie ksdj lajdlksdjfos ksjdie klaskdjfe klsjadi",
+        shortDescription: "Best kdjsai llak...",
+        imageUrl: "https://placebeard.it/400x400",
+        price: $160,
+        prodId: 18287784847748,
+        rating: 3,
+        ratingCount: 9,
+        salePrice: $79
+      },
+]
